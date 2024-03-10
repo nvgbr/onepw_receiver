d5cad9c9752e9a02b649561986abc80b52c4d748
fant5y <vroni@salala.de>
2024-03-10 19:00:32 +0100

    🚀 feat: Add rich configuration for logging and console output This commit adds rich configuration for logging and console output to enhance user experience. The code sets up a custom theme for the console, initializes datetime variables, installs rich traceback, and configures logging with a rich handler. This will improve the readability and visual appeal of the logs and console output for users. -  CHATTY out

---
+++ src/onepw_receiver/rich_configs.py
@@ -0,0 +1,30 @@
+import logging
+from datetime import datetime, timedelta
+
+from rich.console import Console
+from rich.logging import RichHandler
+from rich.theme import Theme
+from rich.traceback import install
+
+custom_theme = Theme(
+    {"success": "grey3 on pale_green1 bold", "error": "grey93 on red bold"}
+)
+
+console = Console(highlight=True, emoji=True, theme=custom_theme, emoji_variant="emoji")
+
+
+today = datetime.now()
+yesterday = today - timedelta(days=1)
+last_30_days = today - timedelta(days=30)
+
+install(show_locals=False)
+
+logger = logging.getLogger("rich")
+
+logging.basicConfig(
+        level="ERROR",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True, tracebacks_show_locals=False)],
+        force=True,
+        )

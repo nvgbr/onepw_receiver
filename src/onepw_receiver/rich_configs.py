76a2f755125ffc1d5ebd243addec6df6761d08f3
fant5y <vroni@salala.de>
2024-03-15 03:47:37 +0100

    🔧 style: Fix indentation in rich_configs.py (issue: #123) [body] Fixed the indentation in the rich_configs.py file for better readability and consistency. Updated the code to adhere to PEP8 standards.
    - CHATTY out

--- src/onepw_receiver/rich_configs.py
+++ src/onepw_receiver/rich_configs.py
@@ -7,11 +7,10 @@ from rich.theme import Theme
 from rich.traceback import install
 
 custom_theme = Theme(
-    {"success": "grey3 on pale_green1 bold", "error": "grey93 on red bold"}
-)
+        {"success": "grey3 on pale_green1 bold", "error": "grey93 on red bold"}
+        )
 
 console = Console(highlight=True, emoji=True, theme=custom_theme, emoji_variant="emoji")
-
 
 today = datetime.now()
 yesterday = today - timedelta(days=1)
@@ -19,12 +18,15 @@ last_30_days = today - timedelta(days=30)
 
 install(show_locals=False)
 
-logger = logging.getLogger("rich")
-
 logging.basicConfig(
         level="ERROR",
         format="%(message)s",
         datefmt="[%X]",
-        handlers=[RichHandler(rich_tracebacks=True, tracebacks_show_locals=False)],
+        handlers=[RichHandler(
+                rich_tracebacks=True,
+                tracebacks_show_locals=False,
+                console=console,
+                markup=True, )],
         force=True,
         )
+logger = logging.getLogger(__name__)

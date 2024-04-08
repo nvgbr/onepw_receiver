62d518c23d0499c05ef3f6a38a9ba2bdb0cb3f4f
fant5y <vroni@salala.de>
2024-04-08 19:08:37 +0200

    🔧 fix(rich_configs): Update logging level to 'ERROR'

--- src/onepw_receiver/rich_configs.py
+++ src/onepw_receiver/rich_configs.py
@@ -17,7 +17,7 @@ last_30_days = today - timedelta(days=30)
 install(show_locals=False)
 
 logging.basicConfig(
-    level="INFO",
+    level="ERROR",
     format="%(message)s",
     datefmt="[%X]",
     handlers=[

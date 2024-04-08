10e2ede89c7838e2bffeba2124b45cc7979993e7
fant5y <vroni@salala.de>
2024-04-08 16:49:52 +0200

    Revert "🔧 refactor: Update pyproject.toml and user settings class"
    This reverts commit afc653e80a0386fa993a539aa3430e04d4c7a016.

--- src/onepw_receiver/__init__.py
+++ src/onepw_receiver/__init__.py
@@ -1,12 +1,2 @@
-import os
-
-from dotenv import load_dotenv, find_dotenv
-
-from .credentials import OnePasswordItem
-from .usersettings import UserSettings
-
-# Load environment variables
-
-environment = os.environ
-load_dotenv()
-find_dotenv()
+from credentials import OnePasswordItem
+from usersettings import UserSettings
--- src/onepw_receiver/credentials.py
+++ src/onepw_receiver/credentials.py
@@ -1,22 +1,19 @@
 # coding=utf-8
-import sys
 import os
-from typing import Dict, Optional
-from dataclasses import dataclass, InitVar, field
+from typing import Dict
 
 from dotenv import load_dotenv
 from onepasswordconnectsdk.client import (
     Client,
     new_client_from_environment,
     )
-from httpx import ConnectTimeout
-from onepw_receiver.rich_configs import logger
+
+from .rich_configs import logger
 
 # Load environment variables
 load_dotenv()
 
 
-@dataclass
 class OnePasswordItem:
     """Class representing an item in 1Password vault.
 
@@ -27,7 +24,7 @@ class OnePasswordItem:
         vault_name (str): The name of the 1Password vault.
         vault_id (str): The ID of the 1Password vault.
         item (str): The ID of the 1Password item.
-        field_name (str): The name of the field of the 1Password item.
+        field_name (str): The name of the field.
         section (str): The name of the section.
         value (str): The value of the field.
 
@@ -39,42 +36,32 @@ class OnePasswordItem:
             Sets an environment variable using the provided key and the instance's 'value' attribute.
     """
 
-    # Load environment variables
-    load_dotenv()
-
+    vault_name: str
+    vault_id: str
     item: str
     field_name: str
-    vault_name: Optional[str]
-    section: Optional[str]
+    section: str
     value: str
-    vault_id: str
+
+    # Load environment variables
+    load_dotenv()
 
     # Creating client using OP_CONNECT_TOKEN and OP_CONNECT_HOST env vars
     client: Client = new_client_from_environment()
 
 
-    def __init__(self, item, field_name='credentials', section=None, vault_name=None):
+    def __init__(self, item, field_name, section=None):
+        self.vault_name = os.getenv('OP_VAULT')
+        self.vault_id = self._get_vault_id()
         self.item = item
         self.field_name = field_name
         self.section = section
-        self.vault_name = os.getenv('OP_VAULT') if vault_name is None else vault_name
-        self.vault_id = self._get_vault_id()
         self.value = self._get_field_value()
 
 
-    def _get_vault_id(self) -> str:
-        """Return the ID of the specified vault name.
-        Args:
-           vault_name (str): The name of the vault whose ID is to be returned.
-        Returns:
-           str: The ID of the specified vault.
-        Raises:
-           Exception: If there is an error while retrieving the vault dictionary.
-        """
-        vault_dict = self._get_vault_dict()
-        if not vault_dict:
-            return
-        return vault_dict.get(self.vault_name, "")
+    def _get_item(self):
+        onepw_item = self.client.get_item(self.item, self.vault_id)
+        return {field.label: field.value for field in onepw_item.fields}
 
 
     def _get_field_value(self):
@@ -95,20 +82,6 @@ class OnePasswordItem:
         return self._get_item().get(self.field_name, self.section)
 
 
-    def _get_item(self):
-        onepw_item = self.client.get_item(self.item, self.vault_id)
-        return {field.label: field.value for field in onepw_item.fields}
-
-
-    def _get_vaults(self):
-        try:
-            return OnePasswordItem.client.get_vaults()
-        except ConnectTimeout as e:
-            logger.error(e)
-            print(f"Connection timed out. Please try again or provide the necessary credential value in an environment variable.")
-            return
-
-
     def _get_vault_dict(self) -> Dict[str, str]:
         """Return a dictionary of available 1Password vault names and their IDs.
         Returns:
@@ -116,11 +89,23 @@ class OnePasswordItem:
         Raises:
            Exception: If there is an error while retrieving vaults.
         """
-        vaults = self._get_vaults()
-        if not vaults:
-            return
+        vaults = OnePasswordItem.client.get_vaults()
         vault_dict = {self.item.name: self.item.id for self.item in vaults}
         return vault_dict
+
+
+    def _get_vault_id(self) -> str:
+        """Return the ID of the specified vault name.
+        Args:
+           vault_name (str): The name of the vault whose ID is to be returned.
+        Returns:
+           str: The ID of the specified vault.
+        Raises:
+           Exception: If there is an error while retrieving the vault dictionary.
+        """
+        vault_dict = self._get_vault_dict()
+        return vault_dict.get(self.vault_name, "")
+
 
     def set_environment_key(self, key: str):
         """Set an environment variable using provided key and instance 'value' attribute.
@@ -142,9 +127,9 @@ class OnePasswordItem:
             The 'value' attribute of the 'OnePasswordItem' instance is assumed to be set before this method is called.
             If 'value' is not set, behavior is undefined.
         """
-        env_key = key.upper()
+        upper_key = key.upper()
         try:
-            os.environ[env_key] = self.value
+            os.environ[upper_key] = self.value
             logger.info(f"Environment Variable: '{key}' has been set.")
         except Exception as e:
             logger.error(f"{e}\nEnvironment Variable: '{key}' could not be set.")

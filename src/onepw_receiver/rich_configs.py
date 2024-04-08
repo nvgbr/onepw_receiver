6380979f15107d9eaea72bd1a9bc0785ea7486ce
fant5y <vroni@salala.de>
2024-04-08 16:50:44 +0200

    🔧 refactor: Update code formatting in rich_configs.py -  CHATTY out

--- src/onepw_receiver/rich_configs.py
+++ src/onepw_receiver/rich_configs.py
@@ -6,9 +6,7 @@ from rich.logging import RichHandler
 from rich.theme import Theme
 from rich.traceback import install
 
-custom_theme = Theme(
-        {"success": "grey3 on pale_green1 bold", "error": "grey93 on red bold"}
-        )
+custom_theme = Theme({"success": "grey3 on pale_green1 bold", "error": "grey93 on red bold"})
 
 console = Console(highlight=True, emoji=True, theme=custom_theme, emoji_variant="emoji")
 
@@ -19,14 +17,17 @@ last_30_days = today - timedelta(days=30)
 install(show_locals=False)
 
 logging.basicConfig(
-        level="ERROR",
-        format="%(message)s",
-        datefmt="[%X]",
-        handlers=[RichHandler(
-                rich_tracebacks=True,
-                tracebacks_show_locals=False,
-                console=console,
-                markup=True, )],
-        force=True,
+    level="ERROR",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[
+        RichHandler(
+            rich_tracebacks=True,
+            tracebacks_show_locals=False,
+            console=console,
+            markup=True,
         )
+    ],
+    force=True,
+)
 logger = logging.getLogger(__name__)
